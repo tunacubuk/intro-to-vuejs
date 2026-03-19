ebab6bdc8a922e1c98e20e90db7924ae2446987a
Tuna Çubuk <cubuktuna@gmail.com>
2026-03-19 18:11:20 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+Vue.createApp({
+
+    data() {
+        return {
+            name: "Tuna",
+            age: 21,
+            job: "Software Engineer",
+            website: "https://vuejs.org/"
+        }
+    }
+
+}).mount('#app')
